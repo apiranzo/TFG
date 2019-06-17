8451e312f62178ba2ff731c2c4da8987f2a05445
apiranzo <38683907+apiranzo@users.noreply.github.com>
2019-06-17 09:34:08 +0200

    Add files via upload

---
+++ Fulla04.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fulla04 : MonoBehaviour
+{
+    private Animator animator; 
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnMouseOver()
+    {
+        animator.SetBool("fulla4", true);
+       
+    }
+
+    private void OnMouseExit()
+    {
+        animator.SetBool("fulla4", false);
+        
+    }
+}
